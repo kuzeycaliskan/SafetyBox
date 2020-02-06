13ec8aae8802ba48bb262b97cbaa1d172586dd08
Kuzey <kuzeycaliskann@gmail.com>
2020-02-07 02:26:54 +0300

    bug fixed

--- RP4_GPIO.py
+++ RP4_GPIO.py
@@ -4,10 +4,12 @@ from time import sleep
 class RP4_GPIO():  # <===
     def __init__(self, Box_Pin):
         super().__init__()
-        #self.setGeometry(0, 0, 500, 250)
         self.Box_Pin = Box_Pin
 
+        self.OpenBox()
+
     def OpenBox(self):
+        print("RP4 Deneme" + str(self.Box_Pin))
         led = LED(self.Box_Pin)
 
         while True:
