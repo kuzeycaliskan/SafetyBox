60a2230d0a6a9e09fe871cdf3aade390ae09072f
Kuzey <kuzeycaliskann@gmail.com>
2020-02-09 00:39:07 +0300

    raspberry pi pin demo

--- RP4_GPIO.py
+++ RP4_GPIO.py
@@ -1,20 +1,24 @@
+import gpiozero
+from gpiozero.pins.mock import MockFactory
 from gpiozero import LED
 from time import sleep
 
 ##you can remove pigpio in Project Interpreter for  ignore localhost:8888 error
 
+gpiozero.Device.pin_factory = MockFactory()
+
 class RP4_GPIO():  # <===
     def __init__(self, Box_Pin):
         super().__init__()
-        self.Box_Pin = Box_Pin
 
-        self.OpenBox()
 
-    def OpenBox(self):
-        print("RP4 Deneme" + str(self.Box_Pin))
-        led = LED(self.Box_Pin)
+        self.OpenBox(Box_Pin)
 
-        while True:
+    def OpenBox(self, Box_Pin):
+        print("RP4 Deneme" + str(Box_Pin))
+        led = LED(Box_Pin)
+
+        for a in range(1,5):
             led.on()
             sleep(1)
             led.off()
